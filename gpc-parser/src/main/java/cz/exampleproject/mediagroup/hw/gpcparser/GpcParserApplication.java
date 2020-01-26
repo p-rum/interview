0d2075e5f6d5a97e944aa82b9ee8461e42c1e476
Petra Rumlová <petra.rumlova@inventi.cz>
2020-01-26 09:31:29 +0100

    Initial commit, created basic java classes  On branch develop  Changes to be committed: 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/AppConfiguration.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/GpcParserApplication.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/PaymentRepository.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/config/AppConfigProperties.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/controller/ParserController.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/exception/BadFormatException.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/exception/FileException.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/exception/ParsingErrorException.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/gpc/GPCPaymentDetails.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/gpc/parser/GPCParser.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/gpc/parser/Parser.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/model/Payment.java 	new file:   gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/model/Transaction.java

---
+++ gpc-parser/src/main/java/cz/exampleproject/mediagroup/hw/gpcparser/GpcParserApplication.java
@@ -0,0 +1,15 @@
+package cz.exampleproject.mediagroup.hw.gpcparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GpcParserApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(GpcParserApplication.class, args);
+
+	}
+
+}
